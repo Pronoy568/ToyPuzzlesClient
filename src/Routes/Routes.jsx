4984c084752eff2aef05754081b01ee2bb7b1cec
Pronoy <pronoydas463@gmail.com>
2023-05-18 12:01:21 +0600

    Navbar Design complete

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
+import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Authentication/Login/Login";
+import Register from "../pages/Authentication/Register/Register";
 
 const Routes = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
+    errorElement: <p>Error</p>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 
--- src/pages/share/Navbar/Navbar.jsx
+++ src/pages/share/Navbar/Navbar.jsx
@@ -1,9 +1,59 @@
 import React from "react";
+import logo from "../../../assets/logo7.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-      <h1>Navbar</h1>
+      <div className="navbar bg-base-200 flex items-center justify-around flex-wrap">
+        <div className="">
+          <img
+            className="rounded-full w-11/12 mx-auto md:w-full"
+            src={logo}
+            alt="logo"
+          />
+        </div>
+        <div className="mt-3 md:mt-0">
+          <ul className="menu menu-horizontal">
+            <li className="mx-1 rounded-full">
+              <Link to="/" className="rounded-full">
+                Home
+              </Link>
+            </li>
+            <li className="mx-1 rounded-full">
+              <Link to="/allToys" className="rounded-full">
+                All Toys
+              </Link>
+            </li>
+            <li className="mx-1 rounded-full">
+              <Link to="/myToys" className="rounded-full">
+                My Toys
+              </Link>
+            </li>
+            <li className="mx-1 rounded-full">
+              <Link to="/addToys" className="rounded-full">
+                Add Toys
+              </Link>
+            </li>
+            <li className="mx-1 rounded-full">
+              <Link to="/blog" className="rounded-full">
+                Blog
+              </Link>
+            </li>
+            <li className="mx-1 rounded-full">
+              <Link to="/login" className="rounded-full">
+                Login
+              </Link>
+            </li>
+            <li className="mx-1 rounded-full">
+              <Link className="rounded-full">Logout</Link>
+            </li>
+            <li className="mx-1 rounded-full">
+              <Link className="rounded-full">Profile</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
